a1cc09ce596fb92dd13c00f1ef5445852f174432
phillychi3 <phillychi3@gmail.com>
2024-11-22 21:29:10 +0800

    feat: add frontend

---
+++ frontend/README.md
@@ -0,0 +1,14 @@
+# create sveltekit+electron
+
+## Developing
+
+```bash
+npm run electron-dev
+```
+
+## Build
+
+```bash
+npm run electron-build
+
+```
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0
+
+	function Click() {
+		count += 1
+	}
+</script>
+
+<img src="/svelte.electron.png" width="200" alt="Svelte logo" />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={Click}>
+	Click me {count} times
+</button>
+
+<a href="/testpage">go to other page</a>
